25ec9783726a276be4a0119aaf77f67a262d56a9
Jack Pope <jack@byramdigitalmedia.com>
2023-12-09 20:33:51 -0500

    first

---
+++ revStr.js
@@ -0,0 +1,12 @@
+let str = "Jack"
+
+function revString(str) {
+  let revStr = ""
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    revStr += str[i]
+  }
+  return revStr
+}
+
+console.log(revString(str))
